d928cea7f52e06c97847594e5d07e2fd9b25e271
Simon Stensgaard <STcena1@hotmail.com>
2025-06-22 20:54:02 +0200

    Created get_values_from functions and analyze_fields

--- src/Stash/Classes/__init__.py
+++ src/Stash/Classes/__init__.py
@@ -2,9 +2,10 @@
 
 from src.Stash.Classes.slots_class import create_slots_cls
 from src.Stash.Classes.tuplex_class import create_tuplex_cls
+from src.Stash.Classes.fieldinfo import FieldInfo
 
 #-------------------- Package Management --------------------
 
-__all__ = ["create_slots_cls", "create_tuplex_cls"]
+__all__ = ["create_slots_cls", "create_tuplex_cls", "FieldInfo"]
 __version__ = "0.0.1"
 __author__ = "HysingerDev"
--- src/Stash/Utils/utility.py
+++ src/Stash/Utils/utility.py
@@ -1,6 +1,7 @@
 #-------------------- Imports --------------------
 
 from typing import Type, List, Optional
+from src.Stash.Classes import FieldInfo
 import inspect
 import warnings
 import types
@@ -23,6 +24,16 @@ def check_cls(cls: Type, allow_fallback: bool) -> List[str]:
     else:
         raise ValueError(f"Class {cls.__name__} must include type annotations to produce fields")
     
+def analyze_fields(cls: Type, allow_fallback: bool) -> List[FieldInfo]:
+    if hasattr(cls, "__annotations__",) and cls.__annotations__:
+        return get_values_from_anno(cls)
+    
+    elif allow_fallback and callable(getattr(cls, "__init__", None)):
+        return get_valuess_from_init(cls)
+            
+    else:
+        raise ValueError(f"Class {cls.__name__} must include type annotations to produce fields")
+
 
 def check_metadata(source_cls: Type, target_cls: Type):
     for attr in ("__doc__", "__module__", "__annotations__", "__qualname__"):
@@ -55,4 +66,39 @@ def preserve_methods(source_cls: Type, target_cls: Type, preserve: Optional[List
                 f"Only methods, staticmethods, classmethods, properties are preserved",
                 UserWarning
             )
+
+def get_values_from_anno(cls: Type) -> list[FieldInfo]:
+    results = []
+    for param_name, param_value in cls.__annotations__.items():
+        if param_name in cls.__dict__:
+            has_default = True
+            default_value = cls.__dict__[param_name]
+        else:
+            has_default = False
+            default_value = None
+        results.append(FieldInfo(
+            value_name=param_name,
+            type_annotation=param_value,
+            has_default=has_default,
+            default_value=default_value
+        ))
+    
+    return results
+
+def get_values_from_init(cls: Type) -> list[FieldInfo]:
+    results = []
+    sig = inspect.signature(cls.__init__)
+    params = list(sig.parameters.values())[1:]
+    for param in params:
+        results.append(FieldInfo(
+            value_name=param.name,
+            type_annotation=param.annotation,
+            has_default=param.default is not inspect.Signature.empty,
+            default_value=param.default if param.default is not inspect.Signature.empty else None
+        ))
+
+    if not results:
+        raise ValueError(f"Class {cls.__name__} has no __init__ parameters to infer fields from.")
+    
+    return results
         
