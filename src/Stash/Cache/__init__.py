f2294007296e4f1e3ad97fd8bb2c349b084ae2ba
Simon Stensgaard <STcena1@hotmail.com>
2025-07-02 15:16:21 +0200

    Deleted memoization.py

--- src/Stash/Cache/__init__.py
+++ src/Stash/Cache/__init__.py
@@ -1,10 +1,9 @@
 #-------------------- Imports --------------------
 
-from src.Stash.Cache.memoization import CacheManager
 from src.Stash.Cache.cache_registry import get_global_cache_manager
 
 #-------------------- Package Management --------------------
 
-__all__ = ["CacheManager", "get_global_cache_manager"]
+__all__ = ["get_global_cache_manager"]
 __version__ = "0.0.1"
 __author__ = "HysingerDev"
